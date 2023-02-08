7a7345d57221a0cbca9f232906f109abdf2925aa
matomugo60 <matomugo60@gmail.com>
2023-02-08 09:57:48 +0300

    Complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
-end
+    case array.length
+    when 0
+      ""
+    when 1
+      array.first
+    when 2
+      array.join(" and ")
+    else
+      last = array.pop
+      array.join(", ") + ", and #{last}"
+    end
+  end
+  
